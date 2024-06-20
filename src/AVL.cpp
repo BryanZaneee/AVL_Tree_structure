98061783fc82e2d8a1333c0605a13a7ddec22279
BryanZaneee <bzane09@gmail.com>
2024-06-20 01:45:42 -0400

    Added various comments to the code to make it more readable and understandable.

--- src/AVL.cpp
+++ src/AVL.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <algorithm>
 
+// Default constructor for TreeNode
 TreeNode::TreeNode() {
     name = "";
     ufid = "";
@@ -9,6 +10,7 @@ TreeNode::TreeNode() {
     right = nullptr;
 }
 
+// Parameterized constructor for TreeNode, initializes member variables with given values
 TreeNode::TreeNode(std::string n, std::string u) {
     name = n;
     ufid = u;
@@ -16,27 +18,32 @@ TreeNode::TreeNode(std::string n, std::string u) {
     right = nullptr;
 }
 
+// Default constructor for AVLTree
 AVLTree::AVLTree() {
     root = nullptr;
 }
 
+// Prints the number of levels (height) in the tree
 void AVLTree::printLevelCount() const {
-    int levels = height(this->root);  // Assuming this->root is the root of your AVL tree
+    int levels = height(this->root);  // Assuming this->root is the root of the AVL tree
     std::cout << levels << std::endl;
 }
 
+// Returns the height of the tree from a given node N
 int AVLTree::height(TreeNode* N) const {
     if (N == nullptr)
         return 0;
     return std::max(height(N->left), height(N->right)) + 1;
 }
 
+// Calculates and returns the balance factor of a given node N
 int AVLTree::getBalanceFactor(TreeNode* N) const {
     if (N == nullptr)
         return 0;
     return height(N->left) - height(N->right);
 }
 
+// Performs a right rotation around a given node y and returns the new root
 TreeNode* AVLTree::rightRotate(TreeNode* y) {
     TreeNode* x = y->left;
     TreeNode* T2 = x->right;
@@ -47,6 +54,7 @@ TreeNode* AVLTree::rightRotate(TreeNode* y) {
     return x;
 }
 
+// Performs a left rotation around a given node x and returns the new root
 TreeNode* AVLTree::leftRotate(TreeNode* x) {
     TreeNode* y = x->right;
     TreeNode* T2 = y->left;
@@ -57,6 +65,7 @@ TreeNode* AVLTree::leftRotate(TreeNode* x) {
     return y;
 }
 
+// Inserts a new node into the tree and rebalances if necessary
 TreeNode* AVLTree::insert(TreeNode* r, TreeNode* new_node) {
     int new_value = std::stoi(new_node->ufid);
     if (r == nullptr) {
@@ -65,9 +74,11 @@ TreeNode* AVLTree::insert(TreeNode* r, TreeNode* new_node) {
         return r;
     }
 
+    // If the new node's value is less than the root's value, insert it in the left subtree
     int r_value = std::stoi(r->ufid);
     if (new_value < r_value) {
         r->left = insert(r->left, new_node);
+    // If the new node's value is greater than the root's value, insert it in the right subtree
     } else if (new_value > r_value) {
         r->right = insert(r->right, new_node);
     } else {
@@ -75,15 +86,22 @@ TreeNode* AVLTree::insert(TreeNode* r, TreeNode* new_node) {
         return r;
     }
 
+    // Perform rotations if the tree is unbalanced
     int bf = getBalanceFactor(r);
+    // Left Left Case
     if (bf > 1 && new_value < std::stoi(r->left->ufid))
         return rightRotate(r);
+
+    // Right Right Case
     if (bf < -1 && new_value > std::stoi(r->right->ufid))
         return leftRotate(r);
+
+    // Left Right Case
     if (bf > 1 && new_value > std::stoi(r->left->ufid)) {
         r->left = leftRotate(r->left);
         return rightRotate(r);
     }
+    // Right Left Case
     if (bf < -1 && new_value < std::stoi(r->right->ufid)) {
         r->right = rightRotate(r->right);
         return leftRotate(r);
@@ -92,34 +110,33 @@ TreeNode* AVLTree::insert(TreeNode* r, TreeNode* new_node) {
     return r;
 }
 
+// Finds and returns the node with the minimum value in the tree
 TreeNode* AVLTree::minValueNode(TreeNode* node) {
     TreeNode* current = node;
-    while (current->left != nullptr) {
+    while (current->left != nullptr)
         current = current->left;
-    }
     return current;
 }
 
+// Deletes a node with a given value from the tree and rebalances if necessary
 TreeNode* AVLTree::deleteNode(TreeNode* r, int v) {
-    if (r == nullptr) {
-        return nullptr;
-    }
-
-    int r_value = std::stoi(r->ufid);
-
-    if (v < r_value) {
+    if (r == nullptr)
+        return r;
+        
+    if (v < std::stoi(r->ufid))
         r->left = deleteNode(r->left, v);
-    } else if (v > r_value) {
+    else if (v > std::stoi(r->ufid))
         r->right = deleteNode(r->right, v);
-    } else {
-        if (r->left == nullptr) {
-            TreeNode* temp = r->right;
-            delete r;
-            return temp;
-        } else if (r->right == nullptr) {
-            TreeNode* temp = r->left;
-            delete r;
-            return temp;
+    else {
+        if (r->left == nullptr || r->right == nullptr) {
+            TreeNode* temp = r->left ? r->left : r->right;
+            if (temp == nullptr) {
+                temp = r;
+                r = nullptr;
+            } else {
+                *r = *temp;
+            }
+            delete temp;
         } else {
             TreeNode* temp = minValueNode(r->right);
             r->ufid = temp->ufid;
@@ -128,15 +145,19 @@ TreeNode* AVLTree::deleteNode(TreeNode* r, int v) {
         }
     }
 
+    if (r == nullptr)
+        return r;
+
     int bf = getBalanceFactor(r);
-    if (bf == 2 && getBalanceFactor(r->left) >= 0)
+    if (bf > 1 && getBalanceFactor(r->left) >= 0)
         return rightRotate(r);
-    else if (bf == 2 && getBalanceFactor(r->left) == -1) {
+    if (bf > 1 && getBalanceFactor(r->left) < 0) {
         r->left = leftRotate(r->left);
         return rightRotate(r);
-    } else if (bf == -2 && getBalanceFactor(r->right) <= -0)
+    }
+    if (bf < -1 && getBalanceFactor(r->right) <= 0)
         return leftRotate(r);
-    else if (bf == -2 && getBalanceFactor(r->right) == 1) {
+    if (bf < -1 && getBalanceFactor(r->right) > 0) {
         r->right = rightRotate(r->right);
         return leftRotate(r);
     }
@@ -144,27 +165,29 @@ TreeNode* AVLTree::deleteNode(TreeNode* r, int v) {
     return r;
 }
 
+// Traverses the tree in inorder and stores the nodes in a vector
 void AVLTree::inorderTraversal(TreeNode* r, std::vector<TreeNode*>& nodes) {
-    if (r == nullptr) {
+    if (r == nullptr)
         return;
-    }
     inorderTraversal(r->left, nodes);
     nodes.push_back(r);
     inorderTraversal(r->right, nodes);
 }
 
+// Removes the nth node in inorder traversal
 void AVLTree::removeInorder(TreeNode*& root, int n) {
     std::vector<TreeNode*> nodes;
     inorderTraversal(root, nodes);
-    if (n >= 0 && n < nodes.size()) {
-        int ufid = std::stoi(nodes[n]->ufid);
-        root = deleteNode(root, ufid);
-        std::cout << "successful" << std::endl;
-    } else {
+    if (n < 0 || n >= nodes.size()) {
         std::cout << "unsuccessful" << std::endl;
+        return;
     }
+    TreeNode* node_to_delete = nodes[n];
+    root = deleteNode(root, std::stoi(node_to_delete->ufid));
+    std::cout << "successful" << std::endl;
 }
 
+// Prints the tree nodes in inorder
 void AVLTree::printInorder(TreeNode* r, bool& isFirst) const {
     if (r == nullptr)
         return;
@@ -175,6 +198,7 @@ void AVLTree::printInorder(TreeNode* r, bool& isFirst) const {
     printInorder(r->right, isFirst);
 }
 
+// Prints the tree nodes in postorder
 void AVLTree::printPostorder(TreeNode* r, bool& isFirst) const {
     if (r == nullptr)
         return;
@@ -185,6 +209,7 @@ void AVLTree::printPostorder(TreeNode* r, bool& isFirst) const {
     isFirst = false;
 }
 
+// Prints the tree nodes in preorder
 void AVLTree::printPreorder(TreeNode* r, bool& isFirst) const {
     if (r == nullptr)
         return;
@@ -195,6 +220,7 @@ void AVLTree::printPreorder(TreeNode* r, bool& isFirst) const {
     printPreorder(r->right, isFirst);
 }
 
+// Prints the nodes at a given level in the tree
 void AVLTree::printGivenLevel(TreeNode* r, int level) const {
     if (r == nullptr)
         return;
@@ -206,6 +232,7 @@ void AVLTree::printGivenLevel(TreeNode* r, int level) const {
     }
 }
 
+// Searches for a node with a given value starting from a given root
 TreeNode* AVLTree::recursiveSearch(TreeNode* r, std::string val) const {
     if (r == nullptr) {
         return nullptr;
@@ -220,6 +247,7 @@ TreeNode* AVLTree::recursiveSearch(TreeNode* r, std::string val) const {
     }
 }
 
+// Searches for a node by its UFID and prints the name
 void AVLTree::searchID(std::string ufid) {
     TreeNode* result = recursiveSearch(this->root, ufid);
     if (result != nullptr) {
@@ -229,6 +257,7 @@ void AVLTree::searchID(std::string ufid) {
     }
 }
 
+// Searches for nodes by their name and prints their UFIDs
 void AVLTree::searchNAME(std::string name) {
     std::vector<std::string> ids;
     searchNAMEHelper(this->root, name, ids);
@@ -241,8 +270,9 @@ void AVLTree::searchNAME(std::string name) {
     }
 }
 
+// Helper function for searchNAME to find nodes by name and store their UFIDs in a vector
 void AVLTree::searchNAMEHelper(TreeNode* r, const std::string& name, std::vector<std::string>& ids) {
-    if (r == NULL) {
+    if (r == nullptr) {
         return;
     }
     searchNAMEHelper(r->left, name, ids);
--- src/AVL.h
+++ src/AVL.h
@@ -11,8 +11,10 @@
 
 class TreeNode {
 public:
+    // Strings to store the name and UFID of the student
     std::string name;
     std::string ufid;
+    // Pointers to the left and right children of the node
     TreeNode* left;
     TreeNode* right;
 
@@ -22,23 +24,28 @@ public:
 
 class AVLTree {
 public:
+    // Pointer to the root of the AVL tree
     TreeNode* root;
 
     AVLTree();
     void printLevelCount() const;
     int height(TreeNode* N) const;
     int getBalanceFactor(TreeNode* N) const;
+    // Treenode Functions
     TreeNode* rightRotate(TreeNode* y);
     TreeNode* leftRotate(TreeNode* x);
     TreeNode* insert(TreeNode* r, TreeNode* new_node);
     TreeNode* minValueNode(TreeNode* node);
     TreeNode* deleteNode(TreeNode* r, int v);
+    // Traversal Functions
     void inorderTraversal(TreeNode* r, std::vector<TreeNode*>& nodes);
     void removeInorder(TreeNode*& root, int n);
+    // Print Functions
     void printInorder(TreeNode* r, bool& isFirst) const;
     void printPostorder(TreeNode* r, bool& isFirst) const;
     void printPreorder(TreeNode* r, bool& isFirst) const;
     void printGivenLevel(TreeNode* r, int level) const;
+    // Search Functions
     TreeNode* recursiveSearch(TreeNode* r, std::string val) const;
     void searchID(std::string ufid);
     void searchNAME(std::string name);
